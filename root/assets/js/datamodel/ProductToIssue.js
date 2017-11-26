b93db77e7971857b055298ddfcfe0b77705b4d46
Robert Niederer <robelli4711@gmail.com>
2017-11-26 14:47:29 +0200

    Change Checkbox & Combobox Size
    Change size and layout
    Bug 419, 420, 421

--- root/assets/js/datamodel/ProductToIssue.js
+++ root/assets/js/datamodel/ProductToIssue.js
@@ -20,7 +20,7 @@ ProductToIssue = {
                 });
 
                 var line = '<div class="col-md-4"><div id="div1" class="checkbox"><input id="id' + element.iid +
-                    '" type="checkbox" onclick=\'merlin.onClickIssue("' + element.iid + '");\'><label id="label1" for="id' + element.iid + '">' + result[0].issue +
+                    '" type="checkbox" onclick=\'merlin.onClickIssue("' + element.iid + '");\' style="height: 20px;width: 20px;"><label id="label1" for="id' + element.iid + '" style="vertical-align: sub;">' + result[0].issue +
                     '</label></div></div>';
 
                 $(target).append(line);
